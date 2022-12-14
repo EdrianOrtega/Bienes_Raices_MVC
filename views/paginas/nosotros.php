bd12bde1488f1a30683a05f6acaa2746afa942f3
EdrianOrtega <inuyasha09889@gmail.com>
2022-12-14 14:29:52 -0600

    agregando la pagina principal y el nosotros

---
+++ views/paginas/nosotros.php
@@ -0,0 +1,30 @@
+<main class="contenedor seccion"> 
+    <h1> Conoce sobre Nosotros </h1> 
+
+    <div class="contenido-nosotros">
+        <div class="imagen">
+            <picture>
+                <source srcset="build/img/nosotros.webp" type="image/webp"> 
+                <source srcset="build/img/nosotros.jpg" type="image/jpeg"> 
+                <img loading="lazy" src="build/img/nosotros.jpg" alt="Sobre Nosotros"> 
+            </picture>
+        </div>
+
+        <div class="texto-nosotros"> 
+            <blockquote> 
+                25 Años de experiencia 
+            </blockquote> 
+
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid veritatis dolor debitis labore, ipsa unde! Quis sint dignissimos est quo velit, eligendi reiciendis similique alias rerum, libero dolorem obcaecati minus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis harum veniam quia, voluptatibus voluptatem praesentium itaque libero tenetur aspernatur neque aliquid ipsam facere porro sint aliquam delectus atque nostrum vitae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            </p> 
+            
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque doloribus aperiam vero illum, assumenda nisi totam ea amet repudiandae hic cum molestias minus voluptate autem impedit commodi, saepe harum at. Lorem ipsum dolor sit amet consectetur adipisicing elit.  </p> 
+        </div> 
+    </div>
+</main>
+
+<section class="contenedor seccion"> 
+    <h1> Más Sobre Nosotros </h1> 
+
+    <?php include 'iconos.php' ?> 
+</section>
